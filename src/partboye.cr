146305f8da58ebffd09c8d7b9206e5fda5027dde
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-25 20:51:13 -0400

    add version flag

--- src/partboye.cr
+++ src/partboye.cr
@@ -1,6 +1,14 @@
-# TODO: Write documentation for `Partboye`
-module Partboye
-  VERSION = "0.1.0"
+require "phreak"
+require "yaml"
+shard = YAML.parse(File.read("./shard.yml"))
 
-  # TODO: Put your code here
+module Partboye
+  Phreak.parse! do |partboye|
+    partboye.banner = "A psuedo partition manager."
+
+    partboye.bind(short_flag: 'v', long_flag: "version",
+      description: "Shows the version of Partboye.") do
+      puts shard["version"]
+    end
+  end
 end
